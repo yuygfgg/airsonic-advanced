dbb0d332b408dadd87be4d26f81a9977c4710ca7
Y.Tory <5343692+kagemomiji@users.noreply.github.com>
2023-01-09 23:17:23 +0900

    change ObjectMapper to JsonMapper.builder to treat deprecated Fixes kagemomiji/airsonic-advanced#55

--- integration-test/src/test/java/org/airsonic/test/Scanner.java
+++ integration-test/src/test/java/org/airsonic/test/Scanner.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FileUtils;
@@ -34,10 +35,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Scanner {
     public static final String SERVER = System.getProperty("DockerTestingHost", "http://localhost:4040");
     public static final String DEFAULT_MUSIC = System.getProperty("DockerTestingDefaultMusicFolder", "/tmp/music");
-    public static final ObjectMapper MAPPER = new ObjectMapper()
+    public static final ObjectMapper MAPPER = JsonMapper.builder()
             .enable(SerializationFeature.WRAP_ROOT_VALUE)
             .enable(DeserializationFeature.UNWRAP_ROOT_VALUE)
-            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .build();
     public static final RestTemplate rest = new RestTemplate();
     static {
         for (int i = 0; i < rest.getMessageConverters().size(); i++) {

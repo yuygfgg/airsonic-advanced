5fde809da41a13682b660b61419ec37aff06fcaf
Y.Tory <5343692+kagemomiji@users.noreply.github.com>
2023-01-11 01:03:28 +0900

    add else statement to authRequest configure steps kagemomiji/airsonic-advanced#64
    Signed-off-by: Y.Tory <5343692+kagemomiji@users.noreply.github.com>

--- airsonic-main/src/main/java/org/airsonic/player/security/RESTRequestParameterProcessingFilter.java
+++ airsonic-main/src/main/java/org/airsonic/player/security/RESTRequestParameterProcessingFilter.java
@@ -103,6 +103,8 @@ public class RESTRequestParameterProcessingFilter extends AbstractAuthentication
             authRequest = new UsernameSaltedTokenAuthenticationToken(username, salt, token);
         } else if (password != null) {
             authRequest = new UsernamePasswordAuthenticationToken(username, password);
+        } else {
+            throw new AuthenticationServiceException("", new APIException(ErrorCode.MISSING_PARAMETER));
         }
 
         authRequest.setDetails(authenticationDetailsSource.buildDetails(request));

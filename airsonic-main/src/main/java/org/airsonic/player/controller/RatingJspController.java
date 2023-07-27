a7d781edd7b562cc1f27f965a91e5a5bf1765763
Clint Checketts <clintchecketts@churchofjesuschrist.org>
2023-07-27 11:33:58 -0600

    Migrate playlist view (and fix now playing/rating)

--- airsonic-main/src/main/java/org/airsonic/player/controller/RatingJspController.java
+++ airsonic-main/src/main/java/org/airsonic/player/controller/RatingJspController.java
@@ -7,12 +7,19 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/ratingJsp")
 public class RatingJspController {
     @GetMapping
     public ModelAndView get(HttpServletRequest request, HttpServletResponse response) {
-        return new ModelAndView("rating", request.getParameterMap());
+        Map<String, Object> model = new HashMap<>();
+        model.put("rating", Integer.parseInt(request.getParameter("rating")));
+        model.put("readOnly", request.getParameter("readOnly"));
+        model.put("id", request.getParameter("id"));
+
+        return new ModelAndView("rating", model);
     }
 }

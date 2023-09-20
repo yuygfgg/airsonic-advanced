bffe4f387f08bf50ba0a54f3747aa6d855ee8e2f
Clint Checketts <clintchecketts@churchofjesuschrist.org>
2023-09-19 19:31:08 -0600

    Fix checkstyle findings

--- airsonic-main/src/main/java/org/airsonic/player/controller/CredentialsManagementController.java
+++ airsonic-main/src/main/java/org/airsonic/player/controller/CredentialsManagementController.java
@@ -22,7 +22,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 

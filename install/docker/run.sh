a18e734fd873fe2678856f22131d5cd2b492f60a
Y.Tory <5343692+kagemomiji@users.noreply.github.com>
2024-02-20 12:10:28 +0000

    kagemomiji/airsonic-advanced#250 revert to war package

--- install/docker/run.sh
+++ install/docker/run.sh
@@ -21,6 +21,7 @@ if [[ $# -lt 1 ]] || [[ ! "$1" == "java"* ]]; then
         java_opts_array+=( "$item" )
     done < <([[ $JAVA_OPTS ]] && xargs printf '%s\0' <<<"$JAVA_OPTS")
     exec java -Xmx256m \
+     -cp /app/WEB-INF/classes:/app/WEB-INF/lib/*:/app/WEB-INF/lib-provided/* \
      -Dserver.address=0.0.0.0 \
      -Dserver.port=$AIRSONIC_PORT \
      -Dserver.servlet.context-path=$CONTEXT_PATH \
@@ -31,8 +32,7 @@ if [[ $# -lt 1 ]] || [[ ! "$1" == "java"* ]]; then
      -DUPnpPort=$UPNP_PORT \
      -Djava.awt.headless=true \
      "${java_opts_array[@]}" \
-     -jar /app/airsonic.jar "$@"
-     #org.airsonic.player.Application "$@"
+     org.airsonic.player.Application "$@"
 fi
 
 exec "$@"
